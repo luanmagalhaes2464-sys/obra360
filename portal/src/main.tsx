2a39904397b39f0250bdbf5e9e396cdff0bd10b2
luanmagalhaes2464-sys <luanmagalhaes2464@gmail.com>
2026-09-14 17:39:55 -0300

    Ativa interface Obra360 PASS

--- portal/src/main.tsx
+++ portal/src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles.css'
+import AppPassV2 from './AppPassV2'
+import './pass.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AppPassV2 />
   </React.StrictMode>,
 )
