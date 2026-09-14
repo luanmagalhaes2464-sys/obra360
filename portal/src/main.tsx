bc3fc55af80b13d75808e93c5ec38f64c6e014c3
luanmagalhaes2464-sys <luanmagalhaes2464@gmail.com>
2026-09-14 20:08:38 -0300

    Ativa copiloto de campo no portal

--- portal/src/main.tsx
+++ portal/src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppOSV3 from './AppOSV3'
+import AutopilotDock from './AutopilotDock'
 import './os-v3.css'
+import './autopilot.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppOSV3 />
+    <AutopilotDock />
   </React.StrictMode>,
 )
