881dcd2bfa9a571fc8a7dfeea0c76eaf7a0d0040
luanmagalhaes2464-sys <luanmagalhaes2464@gmail.com>
2026-09-15 09:02:15 -0300

    Ativa refinamento visual da Visão Geral

--- portal/src/main.tsx
+++ portal/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import AppOSV4 from './AppOSV4'
 import './os-v4.css'
+import './overview-v5.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
