a0b3d5806dfe83cfeb9945a9c512e3efbfe62dc2
luanmagalhaes2464-sys <luanmagalhaes2464@gmail.com>
2026-09-15 20:03:03 -0300

    Remove scripts de voz instáveis e restaura portal responsivo

--- portal/src/main.tsx
+++ portal/src/main.tsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom/client'
 import AppOSV4 from './AppOSV4'
 import './os-v4.css'
 import './overview-v5.css'
-import './voice-enhancer.css'
-import './voice-enhancer'
-import './voice-quality'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
