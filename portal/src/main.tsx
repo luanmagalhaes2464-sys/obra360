3ad70d8186ccd276eef76fbd3903ffc4a8b9d5bd
luanmagalhaes2464-sys <luanmagalhaes2464@gmail.com>
2026-09-15 19:54:08 -0300

    Ativa seleção automática de voz mais natural

--- portal/src/main.tsx
+++ portal/src/main.tsx
@@ -5,6 +5,7 @@ import './os-v4.css'
 import './overview-v5.css'
 import './voice-enhancer.css'
 import './voice-enhancer'
+import './voice-quality'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
