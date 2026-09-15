0a119285a84c3605387552ba19c958452c3f5991
luanmagalhaes2464-sys <luanmagalhaes2464@gmail.com>
2026-09-15 19:18:48 -0300

    Carrega estilo da resposta falada

--- portal/src/main.tsx
+++ portal/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import AppOSV4 from './AppOSV4'
 import './os-v4.css'
 import './overview-v5.css'
+import './voice-enhancer.css'
 import './voice-enhancer'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
