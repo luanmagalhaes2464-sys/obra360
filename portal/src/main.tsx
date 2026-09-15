d8066a426a7ab20d8f2b8c0f40250fd58f4176fe
luanmagalhaes2464-sys <luanmagalhaes2464@gmail.com>
2026-09-15 20:07:22 -0300

    Ativa conversa contínua estável no Copiloto

--- portal/src/main.tsx
+++ portal/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import AppOSV4 from './AppOSV4'
 import './os-v4.css'
 import './overview-v5.css'
+import './voice-conversation-stable'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
