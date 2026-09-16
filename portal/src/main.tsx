185f55a697006de722840ddd5c67e8997393caec
luanmagalhaes2464-sys <luanmagalhaes2464@gmail.com>
2026-09-15 21:53:28 -0300

    Ativa custos conectados e marca TecnoMata

--- portal/src/main.tsx
+++ portal/src/main.tsx
@@ -8,11 +8,15 @@ import './ui-refinements.css'
 import './ui-refinements-v2.css'
 import './stage-planning.css'
 import './matinho-ui.css'
+import './finance-connectivity.css'
+import './tecnomata-brand.css'
 import './voice-conversation-stable'
 import './ui-refinements'
 import './ui-refinements-v2'
 import './stage-planning'
 import './matinho-ui'
+import './finance-connectivity'
+import './tecnomata-brand'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
