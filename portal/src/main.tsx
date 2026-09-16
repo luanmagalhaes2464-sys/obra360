33ea86e642158e413fe5be268c9c94d8b57a8a89
luanmagalhaes2464-sys <luanmagalhaes2464@gmail.com>
2026-09-15 21:02:26 -0300

    Ativa Engenharia Civil e guias técnicos das etapas

--- portal/src/main.tsx
+++ portal/src/main.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppOSV4 from './AppOSV4'
+import AppOSV5 from './AppOSV5'
 import './os-v4.css'
 import './overview-v5.css'
+import './stage-guides.css'
 import './voice-conversation-stable'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <AppOSV4 />
+    <AppOSV5 />
   </React.StrictMode>,
 )
